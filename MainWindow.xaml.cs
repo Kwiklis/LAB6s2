e73c231086a14a57e0c2e198eb7c361d1645bd0f
Kwiklis <ekaterina.brunilina@yande.ru>
2025-09-13 14:19:25 +0700

    dobavila mnogku

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,5 +19,12 @@ namespace LAB6s2
             InitializeComponent();
 
         }
+        public void umn_Click(object sender, RoutedEventArgs e)
+        {
+            if (A.Text.Length > 0 && B.Text.Length > 0)
+            {
+                Result.Content = Convert.ToInt32(A.Text) * Convert.ToInt32(B.Text);
+            }
+        }
     }
 }
